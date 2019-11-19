daf489024437eeed2b783a8b17ee627e6158b652
島田淳一 <>
2019-11-19 14:09:56 +0800

    プロジェクト作成

---
+++ CameraProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraProject
+//
+//  Created by 島田淳一 on 2019/11/19.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
